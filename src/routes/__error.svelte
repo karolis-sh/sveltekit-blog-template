4cfced837972aa1093c33a8e3ac55b459be5dcf8
Karolis Šarapnickis <pastas.k@gmail.com>
2021-11-07 19:57:58 +0200

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>404 - Not Found</title>
+  <meta name="robots" content="noindex,follow" />
+</svelte:head>
+
+<div class="h-full flex items-center">
+  <div class="container mx-auto px-4 pb-20">
+    <div>
+      <h1 class="h1 mb-8">404</h1>
+      <p class="body1 mb-8">Sorry we couldn't find this page.</p>
+      <a href="/" class="button inline-block">GO TO HOME PAGE</a>
+    </div>
+  </div>
+</div>
