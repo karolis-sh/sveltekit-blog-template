890cc43e13746f8aee3c7f3509f6e60e88e8e59b
Karolis Šarapnickis <pastas.k@gmail.com>
2021-11-14 15:41:11 +0200

    use xml-writer

--- src/routes/rss.xml.js
+++ src/routes/rss.xml.js
@@ -1,4 +1,4 @@
-import { Feed } from 'feed';
+import XMLWriter from 'xml-writer';
 import { name } from '$lib/info';
 import { getUrl } from '$lib/utils';
 import { getBlogEntries } from '$lib/content';
@@ -8,43 +8,67 @@ import { getBlogEntries } from '$lib/content';
  */
 export async function get({ host }) {
   const entries = await getBlogEntries();
-  const link = getUrl(host);
 
-  const feed = new Feed({
-    title: name + ' RSS Feed',
-    description: 'This is my personal feed!',
-    id: link,
-    link,
-    language: 'en',
-    // image: 'http://example.com/image.png',
-    // favicon: 'http://example.com/favicon.ico',
-    copyright: 'All rights reserved ' + new Date().getFullYear() + ', ' + name,
-    feedLinks: {
-      atom: getUrl(host, '/rss.xml'),
-    },
-    author: { name, link },
-  });
+  const xw = new XMLWriter();
 
-  entries.forEach(({ metadata: { path, title, description, date, previewHtml } }) => {
-    const url = getUrl(host, path);
+  const el = (tag, children, safe) => {
+    xw.startElement(tag);
+    if (typeof children === 'function') {
+      children();
+    } else {
+      if (children) {
+        if (safe) {
+          xw.text(children);
+        } else {
+          xw.writeCData(children);
+        }
+      }
+    }
 
-    feed.addItem({
-      title: title,
-      id: url,
-      link: url,
-      description: description,
-      content: previewHtml + '\n' + readMore(url),
-      date: new Date(date),
-      // image: post.image,
+    xw.endElement();
+  };
+
+  xw.startDocument('1.0', 'UTF-8');
+  xw.startElement('rss');
+  xw.writeAttribute('xmlns:dc', 'https://purl.org/dc/elements/1.1/');
+  xw.writeAttribute('xmlns:content', 'https://purl.org/rss/1.0/modules/content/');
+  xw.writeAttribute('xmlns:atom', 'https://www.w3.org/2005/Atom');
+  xw.writeAttribute('version', '2.0');
+  el('channel', () => {
+    const title = name + ' RSS Feed';
+    const link = getUrl(host);
+    el('title', title);
+    el('link', link);
+    el('language', 'en', true);
+    el('description', 'Blog posts on stuff');
+    el('image', () => {
+      el('url', link + '/favicon.png', true);
+      el('title', title, true);
+      el('link', link, true);
+    });
+    el('category', 'JavaScript');
+
+    entries.forEach(({ metadata: { path, title, description, date, previewHtml } }) => {
+      const url = getUrl(host, path);
+
+      el('item', () => {
+        el('title', title);
+        el('link', url, true);
+        el('description', description);
+        el('pubDate', new Date(date).toUTCString(), true);
+        el('content:encoded', previewHtml + '\n' + readMore(url));
+      });
     });
   });
+  xw.endElement();
+  xw.endDocument();
 
   return {
     headers: {
       'Cache-Control': 'max-age=0, s-maxage=3600',
       'Content-Type': 'application/xml',
     },
-    body: feed.atom1(),
+    body: xw.toString(),
   };
 }
 
--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,3 +1,4 @@
+import XMLWriter from 'xml-writer';
 import { getEntries } from '$lib/content';
 import { getUrl } from '$lib/utils';
 
@@ -7,28 +8,41 @@ import { getUrl } from '$lib/utils';
 export async function get({ host }) {
   const entries = await getEntries();
 
+  const xw = new XMLWriter();
+
+  const el = (tag, children) => {
+    xw.startElement(tag);
+    if (typeof children === 'function') {
+      children();
+    } else {
+      xw.text(children);
+    }
+    xw.endElement();
+  };
+
+  const addUrl = (url) => {
+    el('url', () => {
+      el('loc', url);
+      el('changefreq', 'daily');
+      el('priority', 0.7);
+    });
+  };
+
+  xw.startDocument('1.0', 'UTF-8');
+  xw.startElement('urlset');
+  xw.writeAttribute('xmlns', 'https://www.sitemaps.org/schemas/sitemap/0.9');
+  entries
+    .map((entry) => entry.metadata.path)
+    .concat(['/blog'])
+    .forEach((path) => addUrl(getUrl(host, path)));
+  xw.endElement();
+  xw.endDocument();
+
   return {
     headers: {
       'Cache-Control': 'max-age=0, s-maxage=3600',
       'Content-Type': 'application/xml',
     },
-    body: sitemap(
-      entries.map((entry) => getUrl(host, entry.metadata.path)).concat([getUrl(host, '/blog')])
-    ),
+    body: xw.toString(),
   };
-}
-
-function sitemap(urls) {
-  return `<?xml version="1.0" encoding="UTF-8" ?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-${urls
-  .map(
-    (url) => `<url>
-  <loc>${url}</loc>
-  <changefreq>daily</changefreq>
-  <priority>0.7</priority>
-</url>`
-  )
-  .join('\n')}
-</urlset>`;
 }
